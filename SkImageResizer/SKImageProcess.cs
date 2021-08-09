0de751671f2bce262771c0e3e12cd54dbabebe9e
minnieliu <minnieliu@changingtec.com>
2021-08-09 10:33:53 +0800

    改成非同步寫法

--- SkImageResizer/SKImageProcess.cs
+++ SkImageResizer/SKImageProcess.cs
@@ -53,9 +53,9 @@ namespace SkImageResizer
                 Directory.CreateDirectory(destPath);
             }
 
-            await Task.Yield();
-
             var allFiles = FindImages(sourcePath);
+            var taskList = new List<Task>();
+
             foreach (var filePath in allFiles)
             {
                 var bitmap = SKBitmap.Decode(filePath);
@@ -68,14 +68,19 @@ namespace SkImageResizer
                 var destinationWidth = (int)(sourceWidth * scale);
                 var destinationHeight = (int)(sourceHeight * scale);
 
-                using var scaledBitmap = bitmap.Resize(
-                    new SKImageInfo(destinationWidth, destinationHeight),
-                    SKFilterQuality.High);
-                using var scaledImage = SKImage.FromBitmap(scaledBitmap);
-                using var data = scaledImage.Encode(SKEncodedImageFormat.Jpeg, 100);
-                using var s = File.OpenWrite(Path.Combine(destPath, imgName + ".jpg"));
-                data.SaveTo(s);
+                taskList.Add(Task.Run(async () =>
+                {
+                    using var scaledBitmap = bitmap.Resize(
+                        new SKImageInfo(destinationWidth, destinationHeight),
+                        SKFilterQuality.High);
+                    using var scaledImage = SKImage.FromBitmap(scaledBitmap);
+                    using var data = scaledImage.Encode(SKEncodedImageFormat.Jpeg, 100);
+                    using var s = File.OpenWrite(Path.Combine(destPath, imgName + ".jpg"));
+                    data.SaveTo(s);
+                }));            
             }
+
+            await Task.WhenAll(taskList);
         }
 
         /// <summary>
